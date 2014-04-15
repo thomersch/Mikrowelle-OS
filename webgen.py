8157844c7b61fa992f63d6dae5d3e69da1c1a4e4
Thomas Skowron <th@skowron.biz>
2014-04-15 22:39:38 +0200

    Paging inkl. Template

--- webgen.py
+++ webgen.py
@@ -4,13 +4,13 @@ __version__ = (1, 3, 0)
 __author__ = "Thomas Skowron (thomersch)"
 
 import util.rssgen as rssgen
-import util.search
 from util.progressbar import AnimatedProgressBar
 
 import sys
 import os
 import json
 import shutil
+import math
 from datetime import datetime
 import distutils.dir_util as du
 
@@ -124,6 +124,31 @@ def _get_elements(posts, settings, format):
 	return elements
 
 
+def _generate_index_pages(posts, settings, formats, index_template):
+	posts_on_page = settings.get("posts_on_page", None)
+	cur_page = 0
+	if posts_on_page is not None:
+		while cur_page*posts_on_page < len(posts):
+			page_posts = posts[posts_on_page*cur_page:posts_on_page*(cur_page+1)]
+			if cur_page == 0:
+				filename = "./tmp_output/index.html"
+			else:
+				filename = "./tmp_output/index.%i.html" % cur_page
+
+			with open(filename, "a+", encoding="utf-8") as f:
+				if (cur_page+1)*posts_on_page >= len(posts):
+					next = None
+				else:
+					next = cur_page+1
+				f.write(index_template.render(posts=page_posts, settings=settings, feeds=formats, index_page=True, next=next))
+			cur_page += 1
+
+	else:
+		# write index.html with all posts
+		with open("./tmp_output/index.html", "a+", encoding="utf-8") as f:
+			f.write(index_template.render(posts=posts, settings=settings, feeds=formats, index_page=True))
+
+
 def json_transform(settings):
 	"""
 		json_transform() transforms auphonic input files into post files
@@ -174,11 +199,7 @@ def generate(settings):
 	for post in posts:
 		write_post(post, settings, single_template, progress)
 
-	# write index.html with all posts
-	index_file_content = index_template.render(posts=posts, settings=settings, feeds=formats, index_page=True)
-
-	with open("./tmp_output/index.html", "a+", encoding="utf-8") as f:
-		f.write(index_file_content)
+	_generate_index_pages(posts, settings, formats, index_template)
 
 	# generate feed_description
 	for fmt in formats:
@@ -194,9 +215,6 @@ def generate(settings):
 		
 		with open("./tmp_output/{}.xml".format(fmt), "wb") as f:
 			f.write(rssgen.generate(channel=channel, elements=elements, settings=settings))
-
-	if settings["search_enabled"]:
-		util.search.generate_search(posts, settings)
 
 	# copy from temp to production and remove tmp
 	du.copy_tree("./tmp_output", publish)
